b2d4587d78682ca062fc1c1af7a202941a309144
XenoCoder2 <brandonewer17951@gmail.com>
2022-03-22 13:23:15 +1100

    Designing Interface

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void ADClick()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
